a1751f91eee62cea45a97a1a23ce38ef4bf6fe40
Robert Peck <rpeck0914@mail.kvcc.edu>
2015-10-01 11:57:42 -0400

    worked on some code

--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/ConversionCalculations.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/ConversionCalculations.java
@@ -4,4 +4,26 @@ package edu.kvcc.cis298.cis298assignment2;
  * Created by Robert Peck on 9/30/2015.
  */
 public class ConversionCalculations {
+
+    private int mFromTemp;
+    private int mToTemp;
+
+    private double mUserInput;
+    private double mConvertedTemp = 0;
+
+    public ConversionCalculations(int fromTemp, int toTemp, double userInput) {
+        mFromTemp = fromTemp;
+        mToTemp = toTemp;
+        mUserInput = userInput;
+        DetermineTempType();
+    }
+
+
+    private  void DetermineTempType() {
+        if (mFromTemp == 0 && mToTemp == 1){
+            mConvertedTemp = ((mUserInput) x 9/5 + 32);
+        }
+    }
+
+
 }
