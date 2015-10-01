1480efcd1f7bbad35a022128f220212d05939096
Robert Peck <rpeck0914@mail.kvcc.edu>
2015-10-01 00:23:53 -0400

    Finished layout, Started on code

---
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/ConversionCalculations.java
@@ -0,0 +1,7 @@
+package edu.kvcc.cis298.cis298assignment2;
+
+/**
+ * Created by Robert Peck on 9/30/2015.
+ */
+public class ConversionCalculations {
+}
--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
@@ -1,16 +1,83 @@
 package edu.kvcc.cis298.cis298assignment2;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class TemperatureConverter extends AppCompatActivity {
+
+    private EditText mUserInput;
+
+    private RadioGroup mFromRadioGroup;
+    private RadioGroup mToRadioGroup;
+
+    private RadioButton mFromCelsius;
+    private RadioButton mFromFahrenheit;
+    private RadioButton mFromKelvin;
+    private RadioButton mFromRankin;
+
+    private RadioButton mToCelsius;
+    private RadioButton mToFahrenheit;
+    private RadioButton mToKelvin;
+    private RadioButton mToRankin;
+
+    private Button mConvertButton;
+
+    private TextView mConversionText;
+    private TextView mEquationText;
+
+    private ConversionCalculations[] mConversions = new ConversionCalculations[]{
+
+
+    };
+
+    private void UpdateSelectedTemps() {
+
+
+    }
+
+    private void CheckSelectedTemps(int fromSelectedId, int toSelectedId){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_converter);
+
+        mUserInput = (EditText) findViewById(R.id.user_input);
+
+        mFromRadioGroup = (RadioGroup) findViewById(R.id.from_radio_group);
+        mToRadioGroup = (RadioGroup) findViewById(R.id.to_radio_group);
+
+        mFromCelsius = (RadioButton) findViewById(R.id.from_celsius);
+        mFromFahrenheit = (RadioButton) findViewById(R.id.from_fahrenheit);
+        mFromKelvin = (RadioButton) findViewById(R.id.from_kelvin);
+        mFromRankin = (RadioButton) findViewById(R.id.from_rankin);
+
+        mToCelsius = (RadioButton) findViewById(R.id.to_celsius);
+        mToFahrenheit = (RadioButton) findViewById(R.id.to_fahrenheit);
+        mToKelvin = (RadioButton) findViewById(R.id.to_kelvin);
+        mToRankin = (RadioButton) findViewById(R.id.to_rankin);
+
+        mConvertButton = (Button) findViewById(R.id.convert_button);
+        mConvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedFromId = mFromRadioGroup.getCheckedRadioButtonId();
+                int selectedToId = mToRadioGroup.getCheckedRadioButtonId();
+
+
+            }
+        });
     }
 
     @Override
